7e82ed0379bccb8cf6434354c0af2ea210880361
MahmoudSZahran <144821514+MahmoudSZahran@users.noreply.github.com>
2023-09-14 02:42:43 +0300

    Update DFF.sv

--- DFF.sv
+++ DFF.sv
@@ -1,11 +1,4 @@
-//  AUTHOR: Mohamed Maged Elkholy.
-//  INFO.: Undergraduate ECE student, Alexandria university, Egypt.
-//  AUTHOR'S EMAIL: majiidd17@icloud.com
-//  FILE NAME: DFF.sv
-//  TYPE: module.
-//  DATE: 28/9/2022
-//  KEYWORDS: D-FlipFlop, Asynchronous reset.
-//  PURPOSE: An RTL modelling for a D-FlipFlop with Asynchronous active low reset.
+//  PURPOSE: An RTL modeling for a D-FlipFlop with Asynchronous active low reset.
 
 module DFF
 //-----------------Ports-----------------\\
